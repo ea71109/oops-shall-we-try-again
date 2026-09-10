e4dcc9060934b8b3d8d81a37d0e51b261c443276
ea71109 <ea71109@mail.harpercollege.com>
2026-09-10 17:23:39 -0500

    Added ValidateInput method and driver file.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,8 +1,19 @@
 package org.example;
+import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
-    // Exercise your `getInput` function here in this driver program.
-    // You can delete these comments.
+    Scanner scanner = new Scanner(System.in);
+
+    int minBound = 1;
+    int maxBound = 100;
+    String prompt = "Please enter a value: ";
+    String errorMessage = "Your value is invalid";
+
+    Oops validator = new Oops();
+    int userChoice = validator.ValidateInput(scanner, minBound, maxBound, prompt, errorMessage);
+
+    System.out.println("The value chosen by the user is " + userChoice);
+    scanner.close();
   }
 }
--- app/src/main/java/org/example/Oops.java
+++ app/src/main/java/org/example/Oops.java
@@ -1,9 +1,35 @@
 package org.example;
+import java.util.Scanner;
 
 public class Oops {
-  public int getInput() {
-    // Implement your function here. You can delete these comments and the "return 0" below.
-    // You will need to fill in the arguments to `getInput` as per the acceptance criteria.
-    return 0;
+  public int ValidateInput(Scanner scanner, int min, int max, String prompt, String errorMessage)
+  {
+    int input = 0;
+    boolean isValid = false;
+
+    while(!isValid)
+    {
+      System.out.println(prompt);
+
+      if (scanner.hasNextInt())
+      {
+        input = scanner.nextInt();
+
+        if (input >= min && input <= max)
+        {
+          isValid = true;
+        }
+        else
+        {
+          System.out.println(errorMessage);
+        }
+      }
+      else
+      {
+        System.out.println(errorMessage);
+        scanner.next();
+      }
+    }
+    return input;
   }
 }
